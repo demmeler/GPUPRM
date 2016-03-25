85045cc1e2672183c6e66a88e5c62ec726d927d5
Manuel Demmeler <manuel.demmeler@tum.de>
2016-03-25 13:33:12 +0100

    ...

--- prm/src/lib/vertexlist3.h
+++ prm/src/lib/vertexlist3.h
@@ -1528,9 +1528,11 @@ public:
       //! insert nodes and edges
       //!
 
-      calc_conn(resbufloc-disp, posqlist, numqlistleft, numqlist, leftconn, rightconn, dsp_, dsp_+cnt_);
+      /*calc_conn(resbufloc-disp, posqlist, numqlistleft, numqlist, leftconn, rightconn, dsp_, dsp_+cnt_);
       calc_conn(resbuf, posqlist, numqlistleft, numqlist, leftconn, rightconn, 0, dsp_);
       calc_conn(resbuf, posqlist, numqlistleft, numqlist, leftconn, rightconn, dsp_+cnt_, num);
+*/
+      calc_conn(resbuf, posqlist, numqlistleft, numqlist, leftconn, rightconn, 0, num);
 
 
 

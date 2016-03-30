b5b149523f6ca71098326b689da1a41b504a5aa6
Manuel Demmeler <manuel.demmeler@tum.de>
2016-03-30 16:32:14 +0200

    ...

--- prm/matlab/lib/pathplot.m
+++ prm/matlab/lib/pathplot.m
@@ -1,16 +1,18 @@
-function [] = pathplot( dhparams, ndof, polys, N, Q, dt, infinite )
+function [] = pathplot( dhparams, ndof, polys, N, Q, dt, infinite)
     
     for i=1:N
         polys{i}.K=convhulln(polys{i}.vertices);
     end
     
-    clf;
+    colors=repmat([1,1,0.5; 0.5,0.5,1],ndof+1,1);
+    
     l=size(Q,2);
     while 1
     for t=[1:l,l:-1:1]
         lim=[xlim;ylim;zlim];
+        
         clf;
-        stateplot(dhparams,ndof,polys,N,Q(:,t));
+        stateplot(dhparams,ndof,polys,N,Q(:,t),colors);
         
         limneu=[xlim;ylim;zlim];
         limneu=[min(limneu(:,1),lim(:,1)), max(limneu(:,2),lim(:,2))];
@@ -24,5 +26,7 @@ function [] = pathplot( dhparams, ndof, polys, N, Q, dt, infinite )
             break;
         end
     end
+    
+    hold off
     
 end
--- prm/matlab/lib/stateplot.m
+++ prm/matlab/lib/stateplot.m
@@ -1,15 +1,18 @@
-function [] = stateplot( dhparams, ndof, polys, N, q )
+function [] = stateplot( dhparams, ndof, polys, N, q , colors)
 % Q= [q1, ...., ql] describing a path
 
     hold on
     
     T=calctrafos(dhparams,ndof,q);
     
+    
     for i=1:N
         V=[polys{i}.vertices,ones(size(polys{i}.vertices,1),1)];
         X=V*T{polys{i}.sys+1}';
         X=X(:,1:3);
-        patch('Faces',polys{i}.K,'Vertices',X,'FaceColor',[1,1,0.5],'FaceAlpha',1.0);
+        patch('Faces',polys{i}.K,'Vertices',X,...
+            'FaceColor',colors(polys{i}.sys+1,:),...
+            'FaceAlpha',1.0);
     end
     
     x=zeros(1,ndof);

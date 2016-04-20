20a0028ecfc7d01b382f17dd2565fbcb572a97d1
Manuel Demmeler <manuel.demmeler@tum.de>
2016-04-20 12:56:20 +0200

    ...

--- prm/src/lib/tictoc.h
+++ prm/src/lib/tictoc.h
@@ -8,6 +8,21 @@
 
 #include "config.h"
 
+#ifdef MPI_CODE
+#define tick(t1) std::chrono::high_resolution_clock::time_point t1=std::chrono::high_resolution_clock::now();
+#define tock(t1) {std::chrono::high_resolution_clock::time_point endtime___=std::chrono::high_resolution_clock::now(); \
+                 int time=std::chrono::duration_cast<std::chrono::microseconds>(endtime___-t1).count();                \
+                 int rank=0; MPI_Comm_rank(MPI_COMM_WORLD, &rank); \
+                 std::cout << rank << ": Since " << #t1 << ": \t" << time << "mcs" << std::endl; }
+#define tockval(t1,time__) {std::chrono::high_resolution_clock::time_point endtime___=std::chrono::high_resolution_clock::now(); \
+                 time__=std::chrono::duration_cast<std::chrono::microseconds>(endtime___-t1).count();                \
+                 int rank=0; MPI_Comm_rank(MPI_COMM_WORLD, &rank); \
+                 std::cout << rank << ": Since " << #t1 << ": \t" << time__ << "mcs" << std::endl; }
+
+#define timedelta(t1,t2) {int time=std::chrono::duration_cast<std::chrono::microseconds>(endtime___-t1).count(); \
+                          int rank=0; MPI_Comm_rank(MPI_COMM_WORLD, &rank); \
+                          std::cout << rank << ": Since " << #t1 << ": \t" << time << "mcs" << std::endl;}
+#else
 #define tick(t1) std::chrono::high_resolution_clock::time_point t1=std::chrono::high_resolution_clock::now();
 #define tock(t1) {std::chrono::high_resolution_clock::time_point endtime___=std::chrono::high_resolution_clock::now(); \
                  int time=std::chrono::duration_cast<std::chrono::microseconds>(endtime___-t1).count();                \
@@ -18,7 +33,7 @@
 
 #define timedelta(t1,t2) {int time=std::chrono::duration_cast<std::chrono::microseconds>(endtime___-t1).count(); \
                           std::cout << "Since " << #t1 << ": \t" << time << "mcs" << std::endl;}
-
+#endif
 
 
 
